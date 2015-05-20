17a4cb15e72c74e3a7c129bd43da4c99712f9185
Egil Mikalsen <egilmikalsen@gmail.com>
2015-05-20 21:31:00 +0200

    src/index.html
    Added bootstrap and revamped look and feel

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,32 @@
 var clicks = 0; //increment this by one every click
-var auto_clicks = 0; //automatically click once per second
+var nrPetriDishes = 1; //automatically click once per second
 var cost = 1; //the cost of this should increase exponentially
 
-
-function update_total_clicks() { //this function updates the number of clicks displayed
-    var e = document.getElementById("total_clicks");
-    e.innerHTML = 'Clicks: ' + clicks;
-}
-
-document.getElementById("click").onclick =    function() {
+/*document.getElementById("click").onclick = function() {
     clicks++;
     update_total_clicks(); //updates the text
-};
+};*/
 document.getElementById("buy_click").onclick =    function() {
     if (clicks < cost) {
-        return alert('need more clicks.');
+        return alert('need more bacteria.');
     }
-    auto_clicks++;
+    nrPetriDishes++;
     clicks -= cost;
-    cost = Math.pow(2, auto_clicks);
-    var e = document.getElementById("clicks_per_second");
-    e.innerHTML = 'Clicks per second: ' + auto_clicks;
-    var e2 = document.getElementById("buy_click");
-    e2.innerHTML = 'Buy an autoclick for ' + cost;
-    update_total_clicks();
+    cost = Math.pow(2, nrPetriDishes);
+    updateBacteriaUI();
 };
 
+function updateBacteriaUI(){
+  var e = document.getElementById("clicks_per_second");
+  e.innerHTML = 'Bacteria per second: ' + nrPetriDishes;
+  var e2 = document.getElementById("buy_click");
+  e2.innerHTML = 'Buy a petri dish for ' + cost;
+  var e = document.getElementById("total_clicks");
+  e.innerHTML = 'Bacteria: ' + clicks;
+
+}
+
 setInterval(function () {
-    clicks += auto_clicks;
-    update_total_clicks();
-}, 1000); //once per second use the auto clickers
+    clicks += nrPetriDishes;
+    updateBacteriaUI();
+}, 1000);
