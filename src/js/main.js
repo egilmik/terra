012a2ea5989d447ba2e6b59e505a7e540cbfb2a9
Egil Mikalsen <egilmikalsen@gmail.com>
2015-05-20 22:07:23 +0200

    src/index.html
    Added loading and saving

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,54 @@
-var clicks = 0; //increment this by one every click
-var nrPetriDishes = 1; //automatically click once per second
-var cost = 1; //the cost of this should increase exponentially
+var player = {
+    bacteria: 0,
+    nrPetriDishes: 1,
+};
 
 /*document.getElementById("click").onclick = function() {
     clicks++;
     update_total_clicks(); //updates the text
 };*/
-document.getElementById("buy_click").onclick =    function() {
-    if (clicks < cost) {
+
+function getPetriDishCost(){
+  return Math.pow(2, player.nrPetriDishes);
+}
+document.getElementById("buy_petri_dish").onclick =    function() {
+    if (player.bacteria < getPetriDishCost()) {
         return alert('need more bacteria.');
     }
-    nrPetriDishes++;
-    clicks -= cost;
-    cost = Math.pow(2, nrPetriDishes);
+    player.bacteria -= getPetriDishCost();
+    player.nrPetriDishes++;
     updateBacteriaUI();
 };
 
 function updateBacteriaUI(){
   var e = document.getElementById("clicks_per_second");
-  e.innerHTML = 'Bacteria per second: ' + nrPetriDishes;
-  var e2 = document.getElementById("buy_click");
-  e2.innerHTML = 'Buy a petri dish for ' + cost;
+  e.innerHTML = 'Bacteria per second: ' + player.nrPetriDishes;
+  var e2 = document.getElementById("buy_petri_dish");
+  e2.innerHTML = 'Buy a petri dish for ' + getPetriDishCost();
   var e = document.getElementById("total_clicks");
-  e.innerHTML = 'Bacteria: ' + clicks;
+  e.innerHTML = 'Bacteria: ' + player.bacteria;
+
+  document.getElementById("nr_pertri_dishes").innerHTML = player.nrPetriDishes;
 
 }
+function loadGame() {
+  if (!localStorage['terra_save']) return;
+    var save_data = JSON.parse(atob(localStorage['terra_save']));
+    player = save_data;
+}
 
+function saveGame() {
+    localStorage['terra_save'] = btoa(JSON.stringify(player));
+}
+
+setInterval(function () { saveGame(); }, 10000);
+
+var firstTime = true;
 setInterval(function () {
-    clicks += nrPetriDishes;
+    if(firstTime){
+      loadGame();
+      firstTime = false;
+    }
+    player.bacteria += player.nrPetriDishes;
     updateBacteriaUI();
 }, 1000);
