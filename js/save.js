35ff32289512dac04f599c81e3ef3b7b021056ca
Egil Mikalsen <egilmikalsen@gmail.com>
2015-05-31 11:23:22 +0200

    Fixed coulmn with for grow tab

---
+++ js/save.js
@@ -0,0 +1,45 @@
+var saveVariable = "terra_save";
+var countVariable = saveVariable + "_producer_count_";
+var upgradeVariable = saveVariable + "_producer_upgrade_list_";
+
+function loadGame() {
+  producerList.forEach(function(entry){
+      var count = parseInt(localStorage[countVariable + entry.id]);
+      if(isNaN(count)){
+        entry.setNumberOwned(0);
+      } else {
+        entry.setNumberOwned(count);
+      }
+      var upgradeList = JSON.parse(localStorage[upgradeVariable + entry.id]);
+      if(upgradeList != null){
+        upgradeList.forEach(function(upgrade){
+          entry.initUpgrade(upgrade);
+        });
+      }
+  });
+}
+
+function saveGame() {
+    producerList.forEach(function(entry){
+      localStorage[ countVariable + entry.id] = entry.getNumberOwned();
+      var upgradeIds = [];
+      entry.boughtUpgrades.forEach(function(upgrade){
+        upgradeIds.push(upgrade.id);
+      });
+      localStorage[upgradeVariable + entry.id] = JSON.stringify(upgradeIds);
+    });
+}
+
+function initPlayer(){
+  player = new EmptyPlayer();
+  producerList.forEach(function(entry){
+    entry.setNumberOwned(0);
+    entry.boughtUpgrades = [];
+  });
+
+}
+
+function clearSave(){
+  initPlayer();
+  saveGame();
+}
